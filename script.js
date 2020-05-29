1e5ef0b0704c5f7e35bdf6a0c9ad6d18f9848e12
victorlee917@gmail.com <41850048+victorlee917@users.noreply.github.com>
2020-05-29 22:54:47 +0900

    commit

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const search = () => {
   let previewTitle = document.getElementById("title");
   let previewDesc = document.getElementById("desc");
   let previewImg = document.getElementById("image");
+  let previewAll = document.getElementById("all");
   const data = {
     key: "5cc819fae5edbab33d7af928bc6bd6ef560d4b4081276",
     q: urlValue,
@@ -17,9 +18,20 @@ const search = () => {
     .then((res) => res.json())
     .then((response) => {
       const { url, title, description, image } = response;
+      let jsonText = `
+      {
+        "image": "${image}",
+        "title": "${title}",
+        "desc": "${description}",
+        "url": "${url}",
+        "category": "",
+        "author": "",
+        "channel": ""
+      },`;
       previewUrl.innerHTML = url;
       previewDesc.innerHTML = description;
       previewImg.innerHTML = image;
       previewTitle.innerHTML = title;
+      previewAll.innerHTML = jsonText;
     });
 };
