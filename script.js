2ff901e458827f51893992fecc4736dfe4bc4a6b
victorlee917@gmail.com <41850048+victorlee917@users.noreply.github.com>
2020-05-31 17:59:57 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const jsonUrl = "https://victorlee917.github.io/cutify/array.json";
+
 const search = () => {
   const urlValue = document.getElementById("urlInput").value;
   let previewUrl = document.getElementById("url");
@@ -9,29 +11,59 @@ const search = () => {
     key: "5cc819fae5edbab33d7af928bc6bd6ef560d4b4081276",
     q: urlValue,
   };
-
-  fetch("https://api.linkpreview.net", {
-    method: "POST",
-    mode: "cors",
-    body: JSON.stringify(data),
-  })
+  fetch(jsonUrl)
     .then((res) => res.json())
-    .then((response) => {
-      const { url, title, description, image } = response;
-      let jsonText = `
-      {
-        "image": "${image}",
-        "title": "${title}",
-        "desc": "${description}",
-        "url": "${url}",
-        "category": "",
-        "author": "",
-        "channel": "youtube"
-      },`;
-      previewUrl.innerHTML = url;
-      previewDesc.innerHTML = description;
-      previewImg.innerHTML = image;
-      previewTitle.innerHTML = title;
-      previewAll.value = jsonText;
+    .then(async (results) => {
+      const checking = (result) => {
+        if (result.url == urlValue) {
+          return new Promise(function (resolve, reject) {
+            resolve(true);
+          });
+        } else {
+          return new Promise(function (resolve, reject) {
+            resolve(false);
+          });
+        }
+      };
+      for (let i = 0; i < results.length; i++) {
+        let result = results[i];
+        const tf = await checking(result);
+        if (tf) {
+          previewUrl.innerHTML = "already exist";
+          previewDesc.innerHTML = "";
+          previewImg.innerHTML = "";
+          previewTitle.innerHTML = "";
+          previewAll.value = "";
+          return;
+        }
+      }
+
+      fetch("https://api.linkpreview.net", {
+        method: "POST",
+        mode: "cors",
+        body: JSON.stringify(data),
+      })
+        .then((res) => res.json())
+        .then((response) => {
+          const { url, title, description, image } = response;
+          let jsonText = `
+            {
+              "image": "${image}",
+              "title": "${title}",
+              "desc": "${description}",
+              "url": "${url}",
+              "category": "",
+              "author": "",
+              "channel": "youtube"
+            },`;
+          previewUrl.innerHTML = url;
+          previewDesc.innerHTML = description;
+          previewImg.innerHTML = image;
+          previewTitle.innerHTML = title;
+          previewAll.value = jsonText;
+        });
+    })
+    .catch((err) => {
+      throw err;
     });
 };
